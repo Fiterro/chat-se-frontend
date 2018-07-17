c3efe30104a4858500528e6cbd9898356ede627f
alexander_sulyaev_cr <alexander.sulyaev.cr@gmail.com>
2018-07-17 16:31:17 +0300

    ### features - messages separated to own component

---
+++ src/app/views/chat/chat-message/chat-message.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+
+import { Message } from "../../../core/classes/message";
+
+@Component({
+    selector: "app-chat-message",
+    templateUrl: "./chat-message.component.html",
+    styleUrls: ["./chat-message.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatMessageComponent {
+    @Input() message: Message;
+}

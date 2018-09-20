413cde7137874fdb574f70b57cb73cb9142d4aef
alexander_sulyaev_cr <alexander.sulyaev.cr@gmail.com>
2018-09-20 15:28:01 +0300

    messages sends through sockets
    ### features
    - sessions support implemented

--- src/app/core/classes/user.ts
+++ src/app/core/classes/user.ts
@@ -1,9 +1,10 @@
 import { UserData } from "../types/user.type";
 import { UserShort } from "../types/user-short.type";
+import { SessionData } from "../types/session.type";
 
 export class User {
     readonly profile: UserShort;
-    readonly session: any;
+    session: SessionData;
 
     constructor(user: UserData) {
         this.profile = user.profile;
